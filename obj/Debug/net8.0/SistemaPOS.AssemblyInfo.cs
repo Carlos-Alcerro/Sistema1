82e4c7b56b5f4b09d8cf5aeb294e0ae8b2521d9f
Carlos <carlosalcerrolainez2017@gmail.com>
2024-07-22 19:02:21 -0600

    Update

--- obj/Debug/net8.0/SistemaPOS.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaPOS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaPOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db05bd10e3e5bd2b169d0ffef38b2da0012c0e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f586e31f60305d3f8eaf6b779e9a88af8982890")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaPOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaPOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
